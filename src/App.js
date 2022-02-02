27cca6be372bb4f7831b1387d2bbc1655d6b23ff
vigneshraaj19 <94341719+vigneshraaj19@users.noreply.github.com>
2022-02-02 18:23:26 +0530

    removed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
+export default function App() {
+  const users=[{names:"vignesh",
+  profile:"https://i.pinimg.com/236x/f7/95/ad/f795adbca930cb8fd25164455d558222--icc-cricket-sachin-tendulkar.jpg"},
+  {names:"Ragul",
+profile:"https://www.forbesindia.com/media/30under30_2018/25_s.jpg"},
+{names:"Akshara",
+  profile:"https://i.pinimg.com/474x/c8/fa/14/c8fa142429b945dea5abe5d2c4a32609--cricket-vini.jpg"},
+  {names:"Ganesh",
+profile:"https://i.pinimg.com/236x/22/45/3e/22453e4e9f7a7d7964279470a81d12b3--ravindra-jadeja-current-events.jpg"}];
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  {users.map(nm=> <Welcome name={nm.names} logo={nm.profile}/>)}
     </div>
   );
 }
-
-export default App;
+function Welcome({name,logo})
+{
+  return(
+    <div className="Welcome">
+      <img className="userinput"
+      src={logo} alt="Logo" />;
+       <h1>Hello, {name}🐱‍🐉🤷‍♂️👍</h1>
+      </div>
+  );
+}
